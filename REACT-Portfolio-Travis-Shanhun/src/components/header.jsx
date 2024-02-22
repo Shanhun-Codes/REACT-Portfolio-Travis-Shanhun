b5e8c3ec9218e7cbfba8bc473967feecc74fa0e3
Raszero <travisjcelli95@gmail.com>
2024-02-22 11:45:48 -0600

    established first connection

---
+++ REACT-Portfolio-Travis-Shanhun/src/components/header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return ( 
+        <header>
+            <h1>hello</h1>
+        </header>
+     );
+}
+ 
+export default Header;
--- REACT-Portfolio-Travis-Shanhun/src/main.jsx
+++ REACT-Portfolio-Travis-Shanhun/src/main.jsx
@@ -1,8 +1,13 @@
+// import react library
 import React from 'react'
+// import reactDom client
 import ReactDOM from 'react-dom/client'
+// import app component
 import App from './App.jsx'
+// import css file
 import './index.css'
 
+// create root element
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
