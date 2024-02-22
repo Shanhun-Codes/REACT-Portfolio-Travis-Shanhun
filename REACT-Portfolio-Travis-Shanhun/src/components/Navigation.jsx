10856c84eb78a83ab2c003c53e9d52d8a283317d
Raszero <travisjcelli95@gmail.com>
2024-02-22 12:37:53 -0600

    discarded inital build imports, completed header

---
+++ REACT-Portfolio-Travis-Shanhun/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+
+
+const  = () => {
+    return ( 
+                    <nav className="nav">
+                <a href="/about"></a>
+                <a href="/"></a>
+                <a href="/"></a>
+                <a href="/"></a>
+            </nav>
+     );
+}
+ 
+export default ;
--- REACT-Portfolio-Travis-Shanhun/src/components/header.jsx
+++ REACT-Portfolio-Travis-Shanhun/src/components/header.jsx
@@ -1,8 +1,15 @@
+import "../../style/main.css"
+import "../../style/header.css"
+
+
 
 const Header = () => {
+    const name = "Travis Shanhun"
+    const slogan = "~Let's Create Tomorrow~"
     return ( 
         <header>
-            <h1>hello</h1>
+            <h1 className="title name-main">{name}</h1>
+            <p className="slogan">{slogan}</p>
         </header>
      );
 }
