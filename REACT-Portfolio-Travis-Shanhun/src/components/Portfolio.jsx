7bf61970eb8fa828e4ff5f0dc74ec32ac52bb961
Raszero <travisjcelli95@gmail.com>
2024-02-23 16:09:31 -0600

    set initial portfolio/project connction

--- REACT-Portfolio-Travis-Shanhun/src/components/Portfolio.jsx
+++ REACT-Portfolio-Travis-Shanhun/src/components/Portfolio.jsx
@@ -1,7 +1,25 @@
-const Portfolio = () => {
+import "../../style/portfolio.css"
+import Project from "./Project";
+
+const Portfolio = () => {   
+    const projectInfo =
+        {
+            title: "ClockWise",
+            description: "Fullstack Web App",
+            deployedLink: "https://nameless-fjord-16797-0078e08b982c.herokuapp.com/login",
+            githubLink: "https://github.com/TimbirICT/ClockWise"
+        }
+        
     return (
-        <section>
-            <h1>Project Page is working</h1>
+        <section className="portfolio-section">
+            <div className="portfolio-container">
+                    <Project 
+                        title={projectInfo.title}
+                        description={projectInfo.description}
+                        deployedLink={projectInfo.deployedLink}
+                        githubLink={projectInfo.githubLink}
+                    />
+            </div>
         </section>
     );
 }
---
+++ REACT-Portfolio-Travis-Shanhun/src/components/Project.jsx
@@ -0,0 +1,23 @@
+import "../../style/project.css"
+
+const Project = (props) => {
+    
+    const iFrameStyle = {
+        border: '0',
+        width: '100%',
+        height: '100%',
+        borderRadius: '10px'
+    };
+
+    return ( 
+        <figure>
+            <label className="title title-adjust">{props.title}</label><br />
+            <p className="description">{props.description}</p>
+            <iframe className="iframe" src={props.deployedLink} allowFullScreen style={iFrameStyle}></iframe><br />
+            <a href={props.deployedLink}>Deployed Application Link</a>
+            <a href={props.githubLink}>GitHub Link</a><br />
+        </figure>
+    );
+};
+ 
+export default Project;
