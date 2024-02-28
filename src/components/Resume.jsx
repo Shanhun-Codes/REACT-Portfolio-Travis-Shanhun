e64ebec99d304cc1cbee2c8b59d28247899e5bda
Raszero <travisjcelli95@gmail.com>
2024-02-28 10:58:38 -0600

    change resume page format

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,10 +8,6 @@ const Resume  = () => {
             <a className="download-btn center" href={resume} download={resume}>Download Resume</a>
             </div>
             <div className="resume-content">
-            <h3 className="title color-change">Technical Skills</h3>
-            <p className="color-change">
-            HTML5, CSS3, JavaScript (ES5+), jQuery (v3.4+), MERN Stack (MongoDB, Express.js, React.js, Node.js), TypeScript, MYSQL, GraphQL, Mongoose and Sequelize ORM, AngularJS, Bootstrap CSS (v4.0+), Materialize CSS (v1.0)
-            </p>
             <h3 className="title color-change">Education</h3>
             <p className="color-change">EdX Full-Stack Web Development Bootcamp - University of Kansas (Sept 2023 - Mar 2024)</p>
            <ul className="color-change">
@@ -20,6 +16,21 @@ const Resume  = () => {
             </li>
             <li className="color-change">Completion Date: 03/12/2024</li>
            </ul>
+            <h3 className="title color-change">Technical Skills</h3>
+                <ul className="color-change">
+                    <li className="color-change">HTML5</li>
+                    <li className="color-change">CSS3</li>
+                    <li className="color-change">JavaScript (ES5+)</li>
+                    <li className="color-change">jQuery (v3.4+)</li>
+                    <li className="color-change">MERN Stack (MongoDB, Express.js, React.js, Node.js)</li>
+                    <li className="color-change">TypeScript</li>
+                    <li className="color-change">MYSQL</li>
+                    <li className="color-change">GraphQL</li>
+                    <li className="color-change">Mongoose and Sequelize ORM</li>
+                    <li className="color-change">Bootstrap CSS (v4.0+)</li>
+                    <li className="color-change">Tailwind CSS (v3.0+)</li>
+                    <li className="color-change">Materialize CSS (v1.0+)</li>
+                </ul>
             <h3 className="title color-change">Proficiencies</h3>
             <ul className="color-change">
                 <li className="color-change">HTML/CSS</li>
