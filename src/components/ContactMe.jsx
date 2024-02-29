3ca5bc3978edfbbc130088dc115aad70c050ddb2
Raszero <travisjcelli95@gmail.com>
2024-02-29 14:36:36 -0600

    submit form debug

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -17,7 +17,7 @@ const ContactMe = () => {
             <a href="mailto:shanhun.codes@gmail.com" className="email">Shanhun.codes@gmail.com</a>
             <h3 className="title">Contact Me!</h3>
             {/*eslint-disable-next-line react/no-unescaped-entities*/}
-            <p className="aussieHey">G'Day! Get in contact with me below!</p>
+            <p className="aussieHey">G&apos;Day! Get in contact with me below!</p>
             <form className="contact-form"
             name="contact"
             method="post"
@@ -30,7 +30,7 @@ const ContactMe = () => {
             <label className="form-name">Name:</label><br />
 
                 {/* input for name  */}
-            <input className="input"
+            <input className="input form-control"
             type="text" 
             name="name" 
             placeholder="Name"
@@ -41,7 +41,7 @@ const ContactMe = () => {
             <label className="form-email">Email:</label><br />
 
                 {/* email input */}
-            <input className="input" 
+            <input className="input form-control" 
             type="email"
             name="email"
             placeholder="Email"
@@ -52,7 +52,7 @@ const ContactMe = () => {
             <label className="form-message">Enter your message here:</label> <br />
 
                 {/* message input */}
-            <textarea className="input text-area" 
+            <textarea className="input text-area form-control" 
             name="message" 
             rows="10" 
             placeholder="Write you message here!! Lets Talk ideas!!!"
