4dfc01d062e9ee2453f978fdfc0958c9d80b2061
Raszero <travisjcelli95@gmail.com>
2024-03-28 10:27:52 -0600

    created seeMore

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,17 +1,38 @@
 import data from "../JSONdata/aboutMeData.json";
 import headShot from "/assets/images/AI-headshot.jpeg";
+import { useState } from "react";
 
 import "../App.css";
 import "../style/aboutMe.css";
 
 const AboutMe = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <section className="aboutMe-section">
-      <img id="headShot" className="hide" src={headShot} alt="Head-shot of Travis Shanhun" />
+      <img
+        id="headShot"
+        className="hide"
+        src={headShot}
+        alt="Head-shot of Travis Shanhun"
+      />
       <h3 className="title aboutMe-sub-title">Professional Summary</h3>
       <p className="professional-summary">{data.proSummaryContext}</p>
       <h3 className="title aboutMe-title aboutMe-sub-title">About Me</h3>
-      <p className="about-me">{data.aboutMeContext}</p>
+      {window.innerWidth <= 768 ? (
+        <>
+          <p>
+            {isExpanded
+              ? data.aboutMeContext
+              : `${data.aboutMeContext.slice(0, 380)}...`}
+          </p>
+          <button id="seeMoreLessButton" onClick={() => setIsExpanded(!isExpanded)}>
+            {isExpanded ? "See Less" : "See More"}
+          </button>
+        </>
+      ) : (
+        <p>{data.aboutMeContext}</p>
+      )}
     </section>
   );
 };
